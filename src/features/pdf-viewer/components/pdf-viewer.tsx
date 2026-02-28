74333807e25eac69dd04debba9520f0b2fa53870
rasulnavir <rismailov@protonmail.com>
2026-02-28 13:19:18 +0400

    render all pdf pages at once

--- src/features/pdf-viewer/components/pdf-viewer.tsx
+++ src/features/pdf-viewer/components/pdf-viewer.tsx
@@ -3,6 +3,7 @@ import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 
+import type { OnDocumentLoadSuccess } from 'react-pdf/src/shared/types.js'
 import { usePdfViewerContext } from '../providers/pdf-viewer-provider'
 import { PopoverCard } from './popover/popover-card'
 import { useHighlightPopover } from './popover/use-highlight-popover'
@@ -13,13 +14,18 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString()
 
 const file = '/sample.pdf'
-const pageNumber = 1
 
 export function PdfViewer() {
     const { popover, clickedHighlight } = usePdfViewerContext()
 
     const { containerRef, handleMouseUp, handleMouseDown } =
         useHighlightPopover()
+
+    // Get the number of pages in the PDF
+    const [numPages, setNumPages] = useState<number>(0)
+    const onDocumentLoadSuccess: OnDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages)
+    }
 
     // Resize the rendered PDF to take the full width of the container
     const [computedDocWidth, setComputedDocWidth] = useState<number>()
@@ -33,31 +39,35 @@ export function PdfViewer() {
     }, [containerRef])
 
     return (
-        <div className="flex flex-col lg:col-span-13">
-            <div
-                ref={containerRef}
-                onMouseUp={handleMouseUp}
-                onMouseDown={handleMouseDown}
-                className="border rounded-md overflow-hidden"
-            >
-                <Document file={file}>
-                    <Page width={computedDocWidth} pageNumber={pageNumber} />
-                </Document>
-
-                {popover && <PopoverCard popover={popover} />}
-
-                {clickedHighlight && (
-                    <PopoverCard
-                        popover={{
-                            x: clickedHighlight.x,
-                            y: clickedHighlight.y,
-                            text: clickedHighlight.payload.text,
-                            rects: clickedHighlight.payload.rects,
-                        }}
-                        userName={clickedHighlight.payload.user.name}
+        <div
+            ref={containerRef}
+            onMouseUp={handleMouseUp}
+            onMouseDown={handleMouseDown}
+            className="border rounded-md overflow-hidden lg:col-span-13"
+        >
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                {Array.from({ length: numPages }, (_, i) => (
+                    <Page
+                        key={i + 1}
+                        width={computedDocWidth}
+                        pageNumber={i + 1}
                     />
-                )}
-            </div>
+                ))}
+            </Document>
+
+            {popover && <PopoverCard popover={popover} />}
+
+            {clickedHighlight && (
+                <PopoverCard
+                    popover={{
+                        x: clickedHighlight.x,
+                        y: clickedHighlight.y,
+                        text: clickedHighlight.payload.text,
+                        rects: clickedHighlight.payload.rects,
+                    }}
+                    userName={clickedHighlight.payload.user.name}
+                />
+            )}
         </div>
     )
 }
